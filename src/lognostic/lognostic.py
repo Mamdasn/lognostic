8d9a664c1e6e71a7447b9aadd125e8be7fbb45a3
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-03-06 22:16:58 +0100

    Add MyPy typing annotations to src py files

--- src/lognostic/lognostic.py
+++ src/lognostic/lognostic.py
@@ -1,24 +1,25 @@
 import logging
+from typing import Dict
 from .stats_manager import StatsManager
 
 
 class Lognostic:
-    def __init__(self):
+    def __init__(self) -> None:
         self._stats_manager = StatsManager()
 
-    def record(self, log_record: logging.LogRecord):
-        logger_name = log_record.name
-        message_size = len(log_record.getMessage())
+    def record(self, log_record: logging.LogRecord) -> None:
+        logger_name: str = log_record.name
+        message_size: int = len(log_record.getMessage())
         self._stats_manager.add_record(logger_name, message_size)
 
-    def total_size(self):
+    def total_size(self) -> int:
         return self._stats_manager.get_total_size()
 
-    def total_size_per_logger(self):
+    def total_size_per_logger(self) -> Dict[str, int]:
         return self._stats_manager.get_total_size_per_logger()
 
-    def total_logging_rate(self, lookback_period=60):
+    def total_logging_rate(self, lookback_period: int = 60) -> float:
         return self._stats_manager.get_total_logging_rate(lookback_period)
 
-    def logging_rate_per_logger(self, lookback_period=60):
+    def logging_rate_per_logger(self, lookback_period: int = 60) -> Dict[str, float]:
         return self._stats_manager.get_logging_rate_per_logger(lookback_period)
--- src/lognostic/stats_manager.py
+++ src/lognostic/stats_manager.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from threading import Lock
+from typing import List, Dict
 
 
 class StatsManager:
-    def __init__(self):
-        self._lock = Lock()
-        self._records = []
+    def __init__(self) -> None:
+        self._lock: Lock = Lock()
+        self._records: List[Dict[str, pd.Timestamp | str | int]] = []
 
-    def add_record(self, logger_name, message_size):
+    def add_record(self, logger_name: str, message_size: int) -> None:
         with self._lock:
             now = pd.Timestamp.now()
             self._records.append(
@@ -18,28 +19,30 @@ class StatsManager:
                 }
             )
 
-    def _dataframe(self):
+    def _dataframe(self) -> pd.DataFrame:
         return pd.DataFrame(self._records)
 
-    def _get_recent_records(self, lookback_period):
+    def _get_recent_records(self, lookback_period: int) -> pd.DataFrame:
         df = self._dataframe()
         time_window = pd.Timestamp.now() - pd.Timedelta(seconds=lookback_period)
         recent = df[df["timestamp"] > time_window]
         return recent
 
-    def get_total_size(self):
+    def get_total_size(self) -> int:
         df = self._dataframe()
         return df["message_size"].sum()
 
-    def get_total_size_per_logger(self):
+    def get_total_size_per_logger(self) -> Dict[str, int]:
         df = self._dataframe()
         return df.groupby("logger_name")["message_size"].sum().to_dict()
 
-    def get_total_logging_rate(self, lookback_period=60):
+    def get_total_logging_rate(self, lookback_period: int = 60) -> float:
         recent_records = self._get_recent_records(lookback_period)
         return recent_records["message_size"].sum() / lookback_period
 
-    def get_logging_rate_per_logger(self, lookback_period=60):
+    def get_logging_rate_per_logger(
+        self, lookback_period: int = 60
+    ) -> Dict[str, float]:
         recent_records = self._get_recent_records(lookback_period)
         return (
             recent_records.groupby("logger_name")["message_size"].sum()
