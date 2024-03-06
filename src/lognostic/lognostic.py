43351d13c6eda0c1f698cb95927bec624bec3a46
Mamdasan Sabrian <mhsaber1375@gmail.com>
2024-03-06 22:47:06 +0100

    Merge `stats_manager` and `lognostic`
    As there is no need for the extra py file.

--- src/lognostic/lognostic.py
+++ src/lognostic/lognostic.py
@@ -1,25 +1,53 @@
 import logging
-from typing import Dict
-from .stats_manager import StatsManager
+import pandas as pd
+from threading import Lock
+from typing import List, Dict
 
 
 class Lognostic:
     def __init__(self) -> None:
-        self._stats_manager = StatsManager()
+        self._lock: Lock = Lock()
+        self._records: List[Dict[str, pd.Timestamp | str | int]] = []
 
     def record(self, log_record: logging.LogRecord) -> None:
         logger_name: str = log_record.name
         message_size: int = len(log_record.getMessage())
-        self._stats_manager.add_record(logger_name, message_size)
+        with self._lock:
+            now = pd.Timestamp.now()
+            self._records.append(
+                {
+                    "logger_name": logger_name,
+                    "message_size": message_size,
+                    "timestamp": now,
+                }
+            )
+
+    def _dataframe(self) -> pd.DataFrame:
+        return pd.DataFrame(self._records)
+
+    def _get_recent_records(self, lookback_period: int) -> pd.DataFrame:
+        df = self._dataframe()
+        time_window = pd.Timestamp.now() - pd.Timedelta(seconds=lookback_period)
+        recent = df[df["timestamp"] > time_window]
+        return recent
 
     def total_size(self) -> int:
-        return self._stats_manager.get_total_size()
+        df = self._dataframe()
+        return df["message_size"].sum()
 
     def total_size_per_logger(self) -> Dict[str, int]:
-        return self._stats_manager.get_total_size_per_logger()
+        df = self._dataframe()
+        return df.groupby("logger_name")["message_size"].sum().to_dict()
 
     def total_logging_rate(self, lookback_period: int = 60) -> float:
-        return self._stats_manager.get_total_logging_rate(lookback_period)
+        recent_records = self._get_recent_records(lookback_period)
+        return recent_records["message_size"].sum() / lookback_period
 
-    def logging_rate_per_logger(self, lookback_period: int = 60) -> Dict[str, float]:
-        return self._stats_manager.get_logging_rate_per_logger(lookback_period)
+    def logging_rate_per_logger(
+        self, lookback_period: int = 60
+    ) -> Dict[str, float]:
+        recent_records = self._get_recent_records(lookback_period)
+        return (
+            recent_records.groupby("logger_name")["message_size"].sum()
+            / lookback_period
+        ).to_dict()
